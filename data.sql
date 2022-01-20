91a1e9aa42a0154745e06801b6993943afc8efbb
nadin210103 <nadu96969@mail.ru>
2022-01-20 15:42:16 +0300

    adding new files

---
+++ data.sql
@@ -0,0 +1,14 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иванов', 'Иванович', 'Иванов', 'suttefranowo-8982@yopmail.com'),
+('Виталий', 'Иванович', 'Иванов', 'cureihennoni-9027@yopmail.com'),
+('Юрий', 'Иванович', 'Иванов', 'sesaujaufremu-9141@yopmail.com'),
+('Анна', 'Барилло', 'Иванова', 'braseiddammuwa-3088@yopmail.com'),
+('Александр', 'Иванович', 'Иванов', 'villoppozocreu-9275@yopmail.com'),
+('Андрей', 'Иванович', 'Иванов', 'socegraupetra-9290@yopmail.com'),
+('Марина', 'Иванович', 'Иванова', 'cimmeucegice-5063@yopmail.com'),
+('Анастасия', 'Иванович', 'Иванова', 'maddeisaufretta-5926@yopmail.com'),
+('Елена', 'Иванович', 'Иванова', 'neiddeugrallaro-6593@yopmail.com'),
+('Никита', 'Иванович', 'Иванов', 'zaffiquetremo-7291@yopmail.com'),
+('Даниил', 'Иванович', 'Иванов', 'pogittequanei-3035@yopmail.com'),
+('Елизавета', 'Иванович', 'Иванова', 'woidepeddouyou-1657@yopmail.com'),
+('Юлия', 'Иванович', 'Иванова', 'bruyehullocra-4737@yopmail.com');
