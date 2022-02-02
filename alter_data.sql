f93a05a580b590a72d52b458e56aec81df41c55f
nadin210103 <nadu96969@mail.ru>
2022-02-02 20:34:36 +0300

    obnovlenie

--- alter_data.sql
+++ alter_data.sql
@@ -52,3 +52,22 @@ insert into role_permission (role_name, permission_id) values
 
 alter table role_permission add constraint FK_USR_ROLE foreign key (permission_id) references permission;
 alter table role_permission add constraint FK_USR_ROLE2 foreign key (role_name) references role;
+
+alter table group_member add constraint FK01 foreign key (university_member_id) references
+university_member;
+alter table group_member add constraint FK02 foreign key (group_id) references
+groups;
+alter table university_member add constraint FK03 foreign key (user_id) references
+usr;
+alter table university_member add constraint FK04 foreign key (department_id) references
+department;
+alter table university_member add constraint FK05 foreign key (positon_id) references
+position;
+update university_member set position_id=1 where id=1;
+update university_member set position_id=2 where id>1;
+update university_member set position_id=3 where id>19;
+update group_member set group_id=1 where id>19;
+update group_member set group_id=2 where id>24;
+update group_member set group_id=3 where id>29;
+update group_member set group_id=4 where id>34;
+update group_member set group_id=5 where id>39;
--- schema.sql
+++ schema.sql
@@ -4,4 +4,38 @@ create table usr (
  first_name varchar(64) not null,
  last_name varchar(64) not null,
  middle_name varchar(64) not null
- ); 
+ );
+create table department (
+ id integer,
+ name varchar(30) not null,
+ primary key (id));
+ );
+create table position (
+ id integer,
+ name varchar(30) not null,
+ primary key (id));
+ );
+create table groups (
+ id integer,
+ name varchar(30) not null,
+ course INTEGER,
+ primary key (id));
+ );
+create table group_member (
+ id integer,
+ university_member_id INTEGER,
+ group_id INTEGER,
+ group_leader char(1);
+ primary key (id));
+ );
+create table university_member (
+ id integer,
+ user_id INTEGER,
+ department_id INTEGER,
+ position_id INTERGER, 
+ primary key (id));
+ );
+
+
+
+
