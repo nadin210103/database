10dc22e52c8b3fc1708b233e39aa5665a428cc1d
nadin210103 <nadu96969@mail.ru>
2022-02-03 21:56:01 +0300

    ok

--- select_data.sql
+++ select_data.sql
@@ -8,3 +8,7 @@ select id, first_name, last_name from usr where length(first_name)>7;
 select * from usr where birth_date>='2003-01-01 00:00:00' order by id;
 select first_name, last_name, birth_date from usr order by birth_date desc;
 select * from usr where (birth_date>='2003-01-01 00:00:00') and (birth_date<='2008-12-31 00:00:00') order by id;
+
+select m.first_name, m.middle_name, m.last_name, m.e_mail, p.name from university_member um join usr m on um.user_id=m.id join position p on um.position_id=p.id
+select m.first_name, m.middle_name, m.last_name, m.e_mail, g.name from group_member gm join university_member um on gm.university_member_id=um.user_id join groups g on gm.group_id=g.id join usr m on um.user_id=m.id;
+select m.first_name, m.middle_name, m.last_name, m.e_mail, d.name from university_member um join usr m on um.user_id=m.id join department d on um.department_id=d.id;
